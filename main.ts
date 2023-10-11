45da901a21427b3e111fc92ab68dd6d11fbb4a54
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-10-11 19:47:28 -0400

    fixes

--- main.ts
+++ main.ts
@@ -14,50 +14,53 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onGesture(Gesture.Shake, function () {
-    randomNumber = randint(0, 2)
-    basic.clearScreen()
+  randomNumber = randint(0, 2)
+  basic.clearScreen()
 
-    // if randomNumber was 0
-    if (randomNumber == 0) {
-      images.createImage(`
+  // if randomNumber was 0
+  if (randomNumber == 0) {
+    basic.showLeds(`
 . . . . .
 . # # # .
 . # # # .
 . # # # .
 . . . . .
-`)
-        }
+`)}
 
     // if randomNumber was 1
     if (randomNumber == 1) {
-       images.createImage(`
+      basic.showLeds(`
 # # # # #
 # . . . #
 # . . . #
 # . . . #
 # # # # #
-`)
-        }
+`)}
     // if randomNumber was 2
     if (randomNumber == 2){
       basic.showIcon(IconNames.Scissors)
-    
     }
 
     // pause and show you are ready again
     basic.pause(1000)
     basic.showIcon(IconNames.Happy)
-    })
+})
 
 // Button A
 input.onButtonPressed(Button.A, function () {
   score = score + 1
-  basic.showString(score.toString())
+  basic.showIcon(IconNames.Yes)
+  basic.pause(1000)
+  basic.showIcon(IconNames.Happy)
+
 })
 
 
 // Button B
 input.onButtonPressed(Button.B, function () {
+  basic.clearScreen()
   basic.showString("Score: ")
   basic.showString(score.toString())
+  basic.pause(1000)
+  basic.showIcon(IconNames.Happy)
 })
