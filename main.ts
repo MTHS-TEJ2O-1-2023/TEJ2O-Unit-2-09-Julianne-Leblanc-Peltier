ecc5f9ecdb530a7c0e1da4e4ca3746008c427b74
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-10-12 12:31:31 -0400

    changed to 5 second wait time

--- main.ts
+++ main.ts
@@ -13,53 +13,53 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onGesture(Gesture.Shake, function () {
-  randomNumber = randint(0, 2)
-  basic.clearScreen()
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
 
-  // if randomNumber was 0
-  if (randomNumber === 0) {
-    basic.showLeds(`
+    // if randomNumber was 0
+    if (randomNumber === 0) {
+        basic.showLeds(`
 . . . . .
 . # # # .
 . # # # .
 . # # # .
 . . . . .
 `)
-  }
+    }
 
-  // if randomNumber was 1
-  if (randomNumber === 1) {
-    basic.showLeds(`
+    // if randomNumber was 1
+    if (randomNumber === 1) {
+        basic.showLeds(`
 # # # # #
 # . . . #
 # . . . #
 # . . . #
 # # # # #
 `)
-  }
-  // if randomNumber was 2
-  if (randomNumber === 2) {
-    basic.showIcon(IconNames.Scissors)
-  }
+    }
+    // if randomNumber was 2
+    if (randomNumber === 2) {
+        basic.showIcon(IconNames.Scissors)
+    }
 
-  // pause and show you are ready again
-  basic.pause(5000)
-  basic.showIcon(IconNames.Happy)
+    // pause and show you are ready again
+    basic.pause(5000)
+    basic.showIcon(IconNames.Happy)
 })
 
 // Button A
 input.onButtonPressed(Button.A, function () {
-  score = score + 1
-  basic.showIcon(IconNames.Yes)
-  basic.pause(1000)
-  basic.showIcon(IconNames.Happy)
+    score = score + 1
+    basic.showIcon(IconNames.Yes)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Happy)
 })
 
 // Button B
 input.onButtonPressed(Button.B, function () {
-  basic.clearScreen()
-  basic.showString('Score: ')
-  basic.showString(score.toString())
-  basic.pause(1000)
-  basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+    basic.showString('Score: ')
+    basic.showString(score.toString())
+    basic.pause(5000)
+    basic.showIcon(IconNames.Happy)
 })
